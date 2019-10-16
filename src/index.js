371bf5bc5b5d5140ca665e5e977c094b26caa6e9
github username <github email address>
2019-10-16 11:02:50 -0400

    challenge

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
 
-  let imageId = 1 //Enter the id from the fetched image here
+  let imageId = 3704
 
-  const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
+  const imageURL = `https://randopic.herokuapp.com/images/`
 
   const likeURL = `https://randopic.herokuapp.com/likes/`
 
   const commentsURL = `https://randopic.herokuapp.com/comments/`
 
-})
+  const imgContainer = document.querySelector("#image"); 
+
+
+  function setImgURL(obj){
+    document.getElementById('image').src = obj.url
+    document.getElementById('name').innerText = obj.name 
+    document.getElementById('like_button').value = obj.like_count
+   }
+
+
+  return fetch(`${imageURL}/${imageId}`) 
+   .then(resp => resp.json()) 
+   .then(json => setImgURL(json))
+
+  })
+
